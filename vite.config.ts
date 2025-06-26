e2b58ff8329a9e5056b4ff47df46689bb25be857
DominikSzczęśniak <dominikszczesniak128@gmail.com>
2025-06-26 13:23:53 +0200

    log

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://java-application-latest-fchh.onrender.com',
+        target: 'https://java-application-latest-d43f.onrender.com',
         changeOrigin: true,
         secure: false,
       }
