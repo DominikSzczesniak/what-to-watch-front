e712eeac72e9a330c31fc289a9cf688c5f751c11
DominikSzczęśniak <dominikszczesniak128@gmail.com>
2025-06-26 13:37:33 +0200

    url change

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://java-application-latest-d43f.onrender.com',
+        target: 'https://java-application-latest-ibso.onrender.com',
         changeOrigin: true,
         secure: false,
       }
